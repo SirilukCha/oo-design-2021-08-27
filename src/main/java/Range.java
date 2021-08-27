0283ffdf5cba558c9e862b52debde7920b1d6d80
Siriluk.Cha <Siriluk.Cha@1000320005469-0>
2021-08-27 16:13:51 +0700

    create function range

--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Range {
+public class Range {
+    private String input;
+
+    public Range(String text) {
+        this.input = text;
+    }
+
+    public boolean isStartWithInclusive() {
+        return this.input.startsWith("[");
+    }
+
+    public boolean isStartWithNumber() {
+        return this.input.startsWith("[");
+    }
 }
--- src/test/java/RangeTest.java
+++ src/test/java/RangeTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RangeTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RangeTest {
+    @Test
+    public void isStartWithInclusiveWithKampooShouldBeTrue () {
+        Range range = new Range ("[1,5]");
+        boolean actualResult = range.isStartWithInclusive();
+        assertTrue(actualResult);
+    }
+
+    @Test
+    public void isStartWithInclusiveWithBracketShouldBeFalse () {
+        Range range = new Range ("(1,5]");
+        boolean actualResult = range.isStartWithInclusive();
+        assertFalse(actualResult);
+    }
 }
